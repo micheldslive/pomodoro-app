38f5deccde4ac406183a8c6a7fc4ff201d82b730
Michel Domingos <micheldslive@gmail.com>
2022-10-23 21:54:07 -0300

    feat: creating test and component Settings

---
+++ src/components/Settings/settings.test.tsx
@@ -0,0 +1,11 @@
+import { getRenderer } from '@/core/tests/helper'
+import Settings from '.'
+
+describe('<Settings />', () => {
+  test('enders the settings pane', () => {
+    const { container } = getRenderer(<Settings visibleTest />)
+    const settingsPane = container.getElementsByClassName('preferences__pane')
+
+    expect(settingsPane.length).toBe(1)
+  })
+})
