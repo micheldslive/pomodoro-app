10c95c419d512e1f9a34862f1104df3bfa3c11c9
Michel Domingos <micheldslive@gmail.com>
2022-10-23 21:50:59 -0300

    feat: creating test and component button

---
+++ src/components/Button/button.test.tsx
@@ -0,0 +1,27 @@
+import { Button } from '.'
+import { expect, test } from 'vitest'
+import { getRenderer } from '@/core/tests/helper'
+
+describe('<Button />', () => {
+  test('renders a close button to close settings', () => {
+    const { getByText } = getRenderer(<Button type='close' buttonText='×' />)
+    const closeButton = getByText(/×/i)
+    expect(closeButton).toBeInTheDocument()
+  })
+
+  it('renders an apply button for settings', () => {
+    const { getByText } = getRenderer(
+      <Button type='apply' buttonText='Apply' />,
+    )
+    const applyButton = getByText(/apply/i)
+    expect(applyButton).toBeInTheDocument()
+  })
+
+  it('renders a button to show settings', () => {
+    const { getByRole } = getRenderer(<Button type='settings' />)
+    const settingsButton = getByRole('button')
+    expect(settingsButton.classList.contains('pomodoro-app__preferences')).toBe(
+      true,
+    )
+  })
+})
