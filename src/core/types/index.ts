b60bbe0a8aa69730b294c895bfcf18ac4833eba3
Michel Domingos <micheldslive@gmail.com>
2022-10-23 21:49:15 -0300

    feat: creating core(types, context, test help and utils)

---
+++ src/core/types/index.ts
@@ -0,0 +1,62 @@
+import React, { SetStateAction } from 'react'
+
+export interface IChildren {
+  children: React.ReactNode
+}
+
+export interface IStatesContext {
+  settingsVisible: boolean
+  timerMode: string
+  pomoLength: number
+  shortLength: number
+  longLength: number
+  fontPref: string
+  accentColor: string
+  secondsLeft: number
+  isActive: boolean
+  buttonText: string
+  volume: number
+}
+
+export interface IStatesDefaultValues extends IStatesContext {
+  setSettingsVisible(settingsVisible: boolean): void
+  setTimerMode(timerMode: string): void
+  setPomoLength(pomoLength: number): void
+  setShortLength(shortLength: number): void
+  setLongLength(longLength: number): void
+  setFontPref(fontPref: string): void
+  setAccentColor(accentColor: string): void
+  setSecondsLeft(secondsLeft: number): void
+  setIsActive(isActive: boolean): void
+  setButtonText(buttonText: string): void
+  setVolume(volume: number): void
+}
+
+export type TActionStatesContext =
+  | { type: 'settingsVisible'; settingsVisible: boolean }
+  | { type: 'timerMode'; timerMode: string }
+  | { type: 'pomoLength'; pomoLength: number }
+  | { type: 'shortLength'; shortLength: number }
+  | { type: 'longLength'; longLength: number }
+  | { type: 'fontPref'; fontPref: string }
+  | { type: 'accentColor'; accentColor: string }
+  | { type: 'secondsLeft'; secondsLeft: number }
+  | { type: 'isActive'; isActive: boolean }
+  | { type: 'buttonText'; buttonText: string }
+  | { type: 'volume'; volume: number }
+
+export interface IMapedNumbers {
+  [key: string]: number
+}
+
+export interface IMapedStrings {
+  [key: string]: string
+}
+
+export interface ISettingsForm {
+  pomodoro: number
+  shortBreak: number
+  longBreak: number
+  font: string
+  color: string
+}
