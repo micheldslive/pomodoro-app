e855cd284959295c02acd55cd63ea57724c61c70
Michel Domingos <micheldslive@gmail.com>
2022-10-23 21:51:13 -0300

    feat: creating test and component header

---
+++ src/components/Header/header.test.tsx
@@ -0,0 +1,11 @@
+import { expect, test } from 'vitest'
+import { getRenderer } from '@/core/tests/helper'
+import Header from '.'
+
+describe('<Header />', () => {
+  test('renders pomodoro in Header title', () => {
+    const { getByText } = getRenderer(<Header title='pomodoro' />)
+    const appTitle = getByText(/pomodoro/i)
+    expect(appTitle).toBeInTheDocument()
+  })
+})
