e6693a1478cb7c9c20a6011ded3ab10c388b2fc7
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-18 15:26:17 -0300

    refactor: changing context to zustand and other general code tweaks

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { StatesProvider } from './core/context'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <StatesProvider>
-      <App />
-    </StatesProvider>
+    <App />
   </React.StrictMode>,
 )
